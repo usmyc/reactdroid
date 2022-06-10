96fa12defe35ff3083f89d69c5a07000abc7c857
usmyc <hllsmtyc@gmail.com>
2022-06-10 17:38:07 +0200

    Hello!

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,20 +1,54 @@
 import { useState } from "react";
 import "./NewEventForm.css";
 
-export default function NewEventForm() {
+export default function NewEventForm({ addEvent, setShowModal }) {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
+  const [location, setLocation] = useState("amsterdam");
+  const resetForm = () => {
+    setTitle("");
+    setDate("");
+    setLocation("amsterdam");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const event = {
+      title,
+      date,
+      location,
+      id: Math.floor(Math.random() * 100000),
+    };
+    console.log(event);
+    addEvent(event);
+    resetForm();
+    setShowModal(false);
+  };
   return (
-    <form className="new-event-form">
-      {/* <label htmlFor="title">Event Title:</label>
-        <input type="text" id="title" /> */}
+    <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
-        <span>Event Title:{title}</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <span>Event Title:</span>
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
       </label>
       <label>
-        <span>Event Date:{date}</span>
-        <input type="date" onChange={(e) => setDate(e.target.value)} />
+        <span>Event Date:</span>
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
+      </label>
+      <label>
+        <span>Event Location:</span>
+        <select onChange={(e) => setLocation(e.target.value)}>
+          <option value="amsterdam">Amsterdam</option>
+          <option value="berlin">Berlin</option>
+          <option value="london">London</option>
+        </select>
       </label>
       <button>Submit</button>
     </form>
